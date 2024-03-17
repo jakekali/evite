cc7c4f30a4092320e5d57415e615dc84a510b9c2
lizocf <lizelle.ocfemia@cooper.edu>
2024-03-17 00:23:51 -0400

    fix invitation model

--- app/models/invitation.py
+++ app/models/invitation.py
@@ -6,6 +6,6 @@ from .envelopes import Envelope
 
 class Invitation(models.Model):
     event = models.ForeignKey(Event, on_delete=models.CASCADE)
-    background = model.ForeignKey(Background, on_delete=models.CASCADE)
-    envelope = model.ForeignKey(Envelope, on_delete=models.CASCADE)
+    background = models.ForeignKey(Background, on_delete=models.CASCADE)
+    envelope = models.ForeignKey(Envelope, on_delete=models.CASCADE)
     card = models.ImageField(upload_to="invitations")
