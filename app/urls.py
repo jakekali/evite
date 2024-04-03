0af9b4441c7f92c302d9215c6ea8ee6acc029bb7
Jacob Khalili <jacobkal18@gmail.com>
2024-04-03 13:24:13 -0400

    new_jacob_new

--- app/urls.py
+++ app/urls.py
@@ -8,5 +8,9 @@ urlpatterns = [
     path('', index.index, name='index'),
     path('myEvents/', event_views.myEvents, name='myEvents'),
     path('createEvent/', event_views.createEvent, name='createEvent'),
+    path('createEvent/background/', event_views.selectBackground, name='createEvent'),
+    path('createEvent/preview/', event_views.eventPreview, name='createEvent'),
+
+
     # path('upload_card/', event_views.upload_card, name='upload_card'),
 ]
--- app/views/event_views.py
+++ app/views/event_views.py
@@ -4,8 +4,45 @@ from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
 from ..models.events import Event
 from ..models.permissions import Permissions
-from django_htmx.http import HttpResponseClientRedirect
+from django_htmx.http import HttpResponseClientRedirect, retarget
 import base64
+
+
+backgrounds = [
+    { "name": "ocean",
+    "url": "https://images.unsplash.com/photo-1682687982468-4584ff11f88a?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    { "name": "forest",
+    "url": "https://plus.unsplash.com/premium_photo-1675355675451-d49606cb8e4a?q=80&w=2664&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    { "name": "mountain",
+        "url": "https://plus.unsplash.com/premium_photo-1673254928968-b6513f32d43b?q=80&w=1471&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    { "name": "turtle",
+        "url": "https://images.unsplash.com/photo-1707343848873-d6a834b5f9b9?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    { "name": "easter hot cross buns with chocolate eggs on a wooden table jesus food coffee cup",
+        "url": "https://plus.unsplash.com/premium_photo-1710267557925-4c05618b8caf?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    { "name": "ocean",
+    "url": "https://images.unsplash.com/photo-1682687982468-4584ff11f88a?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    { "name": "forest",
+    "url": "https://plus.unsplash.com/premium_photo-1675355675451-d49606cb8e4a?q=80&w=2664&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    { "name": "mountain",
+        "url": "https://plus.unsplash.com/premium_photo-1673254928968-b6513f32d43b?q=80&w=1471&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    { "name": "turtle",
+        "url": "https://images.unsplash.com/photo-1707343848873-d6a834b5f9b9?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    { "name": "easter hot cross buns with chocolate eggs on a wooden table jesus food coffee cup",
+        "url": "https://plus.unsplash.com/premium_photo-1710267557925-4c05618b8caf?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    }
+    
+    ]
+
+
 
 def myEvents(request):
     # if not request.user.is_authenticated:
@@ -24,6 +61,8 @@ def myEvents(request):
 def createEvent(request):
     if request.htmx:
         files = (request.FILES)
+
+        # TODO: Add event to database, and return event_id
         print(files)
         print(request.POST.get('event_name'))
         print(request.POST.get('host_name'))
@@ -31,11 +70,24 @@ def createEvent(request):
         print(request.POST.get('description'))
         print(request.POST.get('location'))
 
-        # TODO: Add event to database
+        event_id = 1234
+
+        out = render(request, 'newEvent/background_selection.html', {'loggedIn':  request.user.is_authenticated,
+                                                                    'backgrounds' : backgrounds,
+                                                                    'event_id': event_id})
+        return retarget(out, '#background_selection')
 
     
 
     else:
+        # TODO add background images via database instead of this import
+        return render(request, 'newEvent/index.html', {'loggedIn':  request.user.is_authenticated, 'backgrounds': backgrounds})
+    
+def selectBackground(request):
+    if request.htmx:
+        for key in request.POST:
+            print(key)
+            print(request.POST[key])
 
-        return render(request, 'newEvent/index.html', {'loggedIn':  request.user.is_authenticated})
-
+def eventPreview(request):
+    return render(request, 'inviteView/animation.html', {'loggedIn':  request.user.is_authenticated})
